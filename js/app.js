7193498aa45dd4c882286909c33907abaf0e7f69
Leticia Lopes <107498937+LeticiaLopess@users.noreply.github.com>
2023-03-29 21:19:26 -0300

    math

--- js/app.js
+++ js/app.js
@@ -7,6 +7,17 @@ $('textarea').on('keyup paste', function(){ // levantou a tecla, ativa a funçã
     }, 200);
 });
 
+
+/*
+$('input').on('keyup paste', function(){
+    setTimeout(function() {
+        var input = $('input').val();
+        var multiplica = input + 10 + Math.PI / 23 + 50
+        console.log(multiplica)
+    }, 200);
+}); 
+*/
+
 // se a pessoa copiar e colar com o teclado, ok, mas se a pessoa copiar e colar com o mouse o evento não ativa por ser o keyup, por isso colocamos o paste junto. 
 
 // Mesmo assim, depois que a pessoa cola com o mouse, devemos colocar um setTimeOut pois o paste só ativa depois de milésimos de segundos então a função só deverá ser executada após esse tempo.
